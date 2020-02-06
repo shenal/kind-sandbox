24882cd29ec1e1a4d2ef03a003c2b7f2298c8779
Shenal Silva <shenal.silva@pearson.com>
2020-02-06 13:23:25 +0530

    Fix mv permission

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -17,4 +17,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+sudo mv ./kind /usr/local/bin/kind
