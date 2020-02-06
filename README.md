c41b88c538e7485ad0de85bc3a4a6105d76edd89
Shenal Silva <shenal.silva@pearson.com>
2020-02-06 11:09:36 +0530

    Added installation scripts

---
+++ README.md
@@ -0,0 +1,2 @@
+### KIND(Kubernetes in Docker) sandbox
+Kind-sandbox is a set of scripts to setup the tools and run a kubernetes in docker setup within a few minutes
---
+++ install-ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
