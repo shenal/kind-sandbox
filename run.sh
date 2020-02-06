9e3d087746c0cde0f2db60e658a945b1014c9612
Shenal Silva <shenal.silva@pearson.com>
2020-02-06 12:18:45 +0530

    Added nginx ingress

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ kind get kubeconfig --name=kind > ./kubeconfig
 sed -i 's%server: https://127.0.0.1:[0-9]*%server: https://127.0.0.1:6443%g' ./kubeconfig
 docker exec -it kind-control-plane sh -c "apt-get update && apt-get install -y vim-tiny && mkdir -p /root/.kube" 
 docker cp ./kubeconfig kind-control-plane:/root/.kube/config
+docker exec -it kind-control-plane sh -c "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.27.0/deploy/static/mandatory.yaml && kubectl patch deployments -n ingress-nginx nginx-ingress-controller -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"nginx-ingress-controller\",\"ports\":[{\"containerPort\":80,\"hostPort\":80},{\"containerPort\":443,\"hostPort\":443}]}],\"nodeSelector\":{\"ingress-ready\":\"true\"},\"tolerations\":[{\"key\":\"node-role.kubernetes.io/master\",\"operator\":\"Equal\",\"effect\":\"NoSchedule\"}]}}}}' && kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.27.0/deploy/static/provider/baremetal/service-nodeport.yaml"
 docker exec -it kind-control-plane bash
--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kind delete cluster
 rm -rf ./kubeconfig
-docker system prune -f
+docker container prune -f
