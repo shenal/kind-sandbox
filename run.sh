50ff91b81b869aaf122c554c45f3e86729ac1083
Shenal Silva <shenal.silva@pearson.com>
2020-02-29 22:27:48 +0530

    switch to main vim

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 kind create cluster --config ./kind-multi-node.yml --kubeconfig ./kubeconfig
 kind get kubeconfig --name=kind > ./kubeconfig 
 sed -i 's%server: https://127.0.0.1:[0-9]*%server: https://127.0.0.1:6443%g' ./kubeconfig
-docker exec -it kind-control-plane sh -c "apt-get update && apt-get install -y vim-tiny bash-completion && mkdir -p /root/.kube"
+docker exec -it kind-control-plane sh -c "apt-get update && apt-get install -y vim bash-completion && mkdir -p /root/.kube"
 docker exec -it kind-control-plane bash -c "echo 'source /usr/share/bash-completion/bash_completion' >>~/.bashrc"
 docker exec -it kind-control-plane bash -c "echo 'source <(kubectl completion bash)' >>~/.bashrc"
 docker exec -it kind-control-plane bash -c "mkdir -p /etc/bash_completion.d"
